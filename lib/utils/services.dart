1bac9a6f64ec427a90cd40c58e1d4fad84bbbb13
shystruk <v.stokol@gmail.com>
2020-05-04 03:04:47 -0700

    http error handling

--- lib/utils/services.dart
+++ lib/utils/services.dart
@@ -1,7 +1,57 @@
 import 'dart:ui';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/material.dart';
 
 class Services {
   static Color hexToColor(String color) {
     return Color(int.parse(color.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+}
+
+class HttpHelper {
+  static dynamic getResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return jsonDecode(response.body);
+      case 400:
+        print('BadRequestException');
+        return null;
+      case 401:
+      case 403:
+        print('User is unauthorised');
+        return null;
+      case 500:
+      default:
+        print('Communication with Server: ${response.statusCode}');
+        return null;
+    }
+  }
+
+  static showHttpStatusDialog(dynamic context, String body) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Status'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(body),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      }
+    );
   }
 }
